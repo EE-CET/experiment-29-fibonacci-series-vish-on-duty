060692692b828d76169b0d0fd9d2850e0434f363
Vish_online <vishnuonline2218@gmail.com>
2026-04-13 12:55:03 +0530

    Update Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Fibonacci {
-    
-        // TODO: Read N
-        // TODO: Handle base cases (N=1 -> 0, N=2 -> 1)
-        // TODO: Calculate the Nth number for N > 2
-        // TODO: Print the result
-    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            if (!sc.hasNextInt()) return;
+            int n = sc.nextInt();
+            if (n <= 0) return;
+            if (n == 1) {
+                System.out.println(0);
+                return;
+            }
+            if (n == 2) {
+                System.out.println(1);
+                return;
+            }
+
+            long a = 0, b = 1;
+            long fib = 0;
+            for (int i = 3; i <= n; i++) {
+                fib = a + b;
+                a = b;
+                b = fib;
+            }
+            System.out.println(b);
+        }
+    }
 }
